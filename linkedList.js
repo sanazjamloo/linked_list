97fc83e6c40cb46b7530198e81e0cf56b9b4e0e6
Sanaz <sanaz.jamloo@gmail.com>
2018-03-30 17:16:30 -0700

    Add js file

---
+++ linkedList.js
@@ -0,0 +1,59 @@
+function LinkedList () {
+  var length = 0;
+  var head = null;
+
+  //creating a new node
+  var Node = function (element) {
+    this.element = element;
+    this.next = null;
+  };
+
+  this.size = function (){
+    return length;
+  };
+
+  this.head = function (){
+    return head;
+  };
+
+//Adding a new node
+  this.add = function(element) {
+    var node =  new Node(element);
+    if (head === null){
+      head = node;
+    } else {
+       var currentNode = head;
+
+      while (currentNode.next){
+        currentNode = currentNode.next;
+      }
+
+      currentNode.next = node;
+    }
+      length++;
+  };
+
+// Remove method
+this.remove = function(element){
+  var currentNode = head;
+  var previousNode;
+  if (currentNode.element === element){
+    head = currentNode.next;
+  } else {
+      while (currentNode.element !== element) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+
+      previuosNode.next = currentNode.next;
+  }
+
+  length --;
+};
+
+
+
+
+
+
+}
